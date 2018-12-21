e3a7f0784815030ea978303a350d5527cbd7e42b
Guido <guido@vonrudorff.de>
2018-12-21 15:43:14 +0100

    Extractor for SDF files

---
+++ sdf_to_sssa.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import scipy.spatial
+import numpy as np
+import sys
+
+def get_sssa_components(coordinates):
+    hull = scipy.spatial.ConvexHull(coordinates, qhull_options='QJ')   
+    diameter = max(scipy.spatial.distance.pdist(coordinates[hull.vertices], 'euclidean'))
+
+    return {'diameter': diameter, 'volume': hull.volume, 'area': hull.area}
+
+fn, identifier = sys.argv[1:]
+fh = open(fn)
+molline = 0
+coordinates = []
+name = 'noname'
+for line in fh:
+	molline += 1
+	if molline == 4:
+		numatoms = int(line.strip().split()[0])
+	if molline > 4 and numatoms > 0:
+		coordinates.append(list(map(float, line.strip().split()[:3])))
+		numatoms -= 1
+	if molline > 4 and numatoms == 0:
+		if line.strip() == '> <%s>' % identifier:
+			name = line.strip()[3:-1]
+		if line.strip() == '$$$$':
+			res = get_sssa_components(np.array(coordinates))
+			print (name, res['volume'], res['area'], res['diameter'])
+			coordinates = []
+			molline = 0
+			name = 'noname'
